91890e8fb7f9708469ca399d8c037200b3ce3e20
Timilehin Bakare <timilehinbakare.tb@gmail.com>
2023-03-07 21:49:40 +0100

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,114 @@
-import React from 'react'
-import { Container } from './About'
+import React from "react";
+import { Container } from "./About";
+import styled from "styled-components";
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import { socialMedia } from "../Socials";
+import { BsLinkedin } from "react-icons/bs";
 
+const Cont = styled.div`
+  .container {
+    width:58%;
+    display:grid;
+    grid-template-columns:30% 58%;
+    gap:12%;
+  }
+  .options {
+    display: flex;
+    flex-direction: column;
+    gap:1.2rem;
+  }
+  .option {
+    background-color: var(--color-bg-variant);
+    padding:1.2rem;
+    border-radius:1.2rem;
+    text-align: center;
+    border:1px solid transparent;
+    transition: var(--transition);
+    a{
+      margin-top:0.7rem;
+      display: inline-block;
+      font-size:0.8rem;
+    }
+  }
+  .option:hover {
+    background-color: transparent;
+    border-color: var(--color-primary-variant);
+  }
+  .icon{
+    font-size:1.5rem;
+    margin-bottom:0.5rem;
+  }
+
+form{
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+}
+input,textarea{
+  width: 100%;
+  padding: 1.2rem;
+  border-radius:0.5rem;
+  background: transparent;
+  border: 2px solid var(--color-primary-variant);
+  resize: none;
+  color: var(--color-white);
+
+}
+@media screen and (max-width:1024px){
+  margin-top:5rem;
+.container{
+
+  grid-template-columns: 1fr;
+  gap: 2rem;
+}
+  }
+  @media screen and (max-width:600px){
+.container{
+  width: var(--container-width-sm)
+}
+  }
+
+`;
 function Contact() {
   return (
-    <Container id='contact'>
-contact
+    <Container id="contact">
+      <Cont>
+        <section>
+          <h5>Get in Touch</h5>
+          <h2>Contact me</h2>
+          <div className="container">
+            <div className="options">
+              <article className="option">
+                <MdOutlineEmail  className="icon"/>
+                <h4>Email</h4>
+                <h5>{socialMedia.gmail}</h5>
+                <a href='mailto:timilehinbakare.tb.com' target="_blank" rel="noreferrer">Send a Mail</a>
+              </article>
+              <article className="option">
+                <BsLinkedin className="icon"/>
+                <h4>linkedIn</h4>
+                <h5>Timilehin Bakare</h5>
+                <a href={socialMedia.linkedIn} target="_blank" rel="noreferrer">Lets connect</a>
+              </article>
+              <article className="option">
+                <BsWhatsapp className="icon"/>
+                <h4>Whatsapp</h4>
+                <h5>{socialMedia.number}</h5>
+                <a href={"tel:" + socialMedia.number} target="_blank" rel="noreferrer" >Contact Me</a>
+              </article>
+            </div>
+            <form  action="https://formsubmit.co/timilehinbakare.tb@gmail.com" method="POST">
+              <input type="text" name="name" placeholder="Your Full Name" required/>
+              <input type="email" name="email" placeholder="Enter your Email" required />
+              <textarea name="message" placeholder="Your Message" required rows="7" ></textarea>
+              <button type="submit" className="btn btn-primary">Send Message</button>
+            </form>
+          </div>
+        </section>
+      </Cont>
     </Container>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
-import { Container } from './About'
-
+import React from "react";
+import { Container } from "./About";
+import styled from "styled-components";
+import { socialMedia } from "../Socials";
+import me from "../Assets/me.png";
+const Port = styled.div`
+  .cont {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap:2.5rem;
+  }
+  .port-item {
+    background:var(--color-bg-variant);
+    padding:1.3rem;
+    border-radius:2rem;
+    border:1px solid transparent;
+    transition:var(--transition);
+    h3{
+      margin:1.2rem 0 2rem;
+    }
+  }
+  .port-item:hover{
+    border-color:var(--color-primary-variant);
+    background: transparent;
+  }
+  .item-img{
+    border-radius:1.5rem;
+    overflow:hidden;
+  }
+  .item-cta{
+    display:flex;
+    gap:1rem;
+    margin-bottom:1rem;
+  }
+  @media screen and (max-width:1024px){
+    margin-top:5rem;
+.cont{
+  grid-template-columns: 1fr 1fr;
+  gap: 1.2rem;
+}
+  }
+  @media screen and (max-width:600px){
+.cont{
+  grid-template-columns:  1fr;
+  gap:1rem
+}
+  }
+`;
 function Portfolio() {
+  const items = [{}, {}, {}, {}, {}, {}];
   return (
-    <Container id ='portfolio'>
-      portfolio
+    <Container id="portfolio">
+      <Port>
+        <section>
+          <h5>My Recent Works</h5>
+          <h2>Portfolio</h2>
+          <div className="container cont">
+            {items.map((item) => {
+              return (
+                <article className="port-item" key={item.id}>
+                  <div className="item-img">
+                    <img src={me} alt="no img" />
+                  </div>
+                  <h3>THis is a portfoilio item title</h3>
+                  <div className="item-cta">
+                   <a href={socialMedia.Github} className="btn">
+                    Github
+                  </a>
+                  <a href="" target="_blank" className="btn btn-primary">
+                    Live Demo
+                  </a>  
+                  </div>
+                 
+                </article>
+              );
+            })}
+          </div>
+        </section>
+      </Port>
     </Container>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
